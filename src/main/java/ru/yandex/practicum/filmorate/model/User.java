8a68b8a65b4fb07d04bdaed40be47c683c292d07
EvgenyRodionov <globevgeny@yandex.ru>
2023-07-10 14:34:32 +0300

    fix: тесты гитхаба

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,24 +2,24 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
 import ru.yandex.practicum.filmorate.validation.ValidUserId;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
-
 @Data
 @AllArgsConstructor
 public class User {
 
+    @ValidUserId(message = "Пользователь с данным id не найден.")
+    private int id;
     @Email(message = "Введен некорректный email.")
     private final String email;
     @NotEmpty(message = "Введен пустой логин.")
-    private final String login;
+    private final  String login;
+    private String name;
     @Past(message = "Введена недопустимая дата рождения.")
     private final LocalDate birthday;
-    @ValidUserId(message = "Пользователь с данным id не найден.")
-    private int id;
-    private String name;
 }
