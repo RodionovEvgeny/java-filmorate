99be55c8d7a27c38a3fd2e7dcc8e29d2f749b870
EvgenyRodionov <globevgeny@yandex.ru>
2023-07-10 14:36:49 +0300

    fix: тесты гитхаба

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,13 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import ru.yandex.practicum.filmorate.validation.ValidUserId;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 public class User {
