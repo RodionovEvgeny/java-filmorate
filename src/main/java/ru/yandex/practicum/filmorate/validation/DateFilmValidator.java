6c34729eed22676f5571497036ffbc8840dadc30
EvgenyRodionov <globevgeny@yandex.ru>
2023-07-27 11:45:46 +0300

    fix: убрать лишние импорты

--- src/main/java/ru/yandex/practicum/filmorate/validation/DateFilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/DateFilmValidator.java
@@ -1,13 +1,12 @@
 package ru.yandex.practicum.filmorate.validation;
 
-import ru.yandex.practicum.filmorate.model.Film;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
 public class DateFilmValidator implements ConstraintValidator<ValidDateFilm, LocalDate> {
     private static final LocalDate FIRST_FILM_DATE = LocalDate.of(1895, 12, 28);
+
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
         return localDate.isAfter(FIRST_FILM_DATE);
