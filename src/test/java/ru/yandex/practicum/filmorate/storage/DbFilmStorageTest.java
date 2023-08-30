bcb477c6ccec4d0d8f2159a73c35950b0fa97d1c
EvgenyRodionov <globevgeny@yandex.ru>
2023-08-30 15:49:33 +0300

    style: удалить лишний импорт в тестах

--- src/test/java/ru/yandex/practicum/filmorate/storage/DbFilmStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/DbFilmStorageTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
